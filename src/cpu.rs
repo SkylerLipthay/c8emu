8f50a343a4ee2cdb643e1c9fe13dbdfb20414c07
Skyler Lipthay <sl@skylerlipthay.com>
2020-02-26 18:16:39 -0700

    Slim down opcode branch code

--- src/cpu.rs
+++ src/cpu.rs
@@ -52,122 +52,82 @@ impl Cpu {
 
     pub fn step(&mut self) {
         assert!(self.pc as usize + 1 < MEM_SIZE, "program counter overflow");
-        let opcode_high = self.memory[self.pc as usize];
-        let opcode_low = self.memory[(self.pc + 1) as usize];
-        let opcode: u16 = ((opcode_high as u16) << 8) | opcode_low as u16;
-        let vx = ((opcode & 0x0f00) >> 8) as usize;
-        let vy = ((opcode & 0x00f0) >> 4) as usize;
-        let nn = (opcode & 0x00ff) as u8;
-        let nnn = opcode & 0x0fff;
+        let op_high = self.memory[self.pc as usize];
+        let op_low = self.memory[(self.pc + 1) as usize];
+        let op: u16 = ((op_high as u16) << 8) | op_low as u16;
+        let vx = ((op & 0x0f00) >> 8) as usize;
+        let vy = ((op & 0x00f0) >> 4) as usize;
+        let n = (op & 0x000f) as u8;
+        let nn = (op & 0x00ff) as u8;
+        let nnn = op & 0x0fff;
 
-        match opcode & 0xf000 {
-            0x0000 if opcode == 0x00e0 => {
+        self.pc += 2;
+
+        match ((op & 0xf000) >> 12, (op & 0x0f00) >> 8, (op & 0x00f0) >> 4, op & 0x000f) {
+            (0x0, 0x0, 0xe, 0x0) => {
                 self.screen.copy_from_slice(&[0; SCREEN_SIZE]);
                 self.should_draw = true;
-                self.pc += 2;
             },
-            0x0000 if opcode == 0x00ee => {
+            (0x0, 0x0, 0xe, 0xe) => {
                 assert!(self.sp > 0, "stack pointer underflow");
                 self.sp -= 1;
                 self.pc = self.stack[self.sp as usize] + 2;
             },
-            0x1000 => {
-                self.pc = nnn;
-            },
-            0x2000 => {
+            (0x1, _, _, _) => self.pc = nnn,
+            (0x2, _, _, _) => {
                 assert!((self.sp as usize) < STACK_SIZE, "stack pointer overflow");
-                self.stack[self.sp as usize] = self.pc;
+                self.stack[self.sp as usize] = self.pc - 2;
                 self.sp += 1;
                 self.pc = nnn;
             },
-            0x3000 => {
-                self.pc += if self.v[vx] == nn { 4 } else { 2 };
-            },
-            0x4000 => {
-                self.pc += if self.v[vx] != nn { 4 } else { 2 };
-            },
-            0x5000 => {
-                self.pc += if self.v[vx] == self.v[vy] { 4 } else { 2 };
-            },
-            0x6000 => {
-                self.v[vx] = nn;
-                self.pc += 2;
-            },
-            0x7000 => {
-                self.v[vx] = self.v[vx].wrapping_add(nn);
-                self.pc += 2;
-            },
-            0x8000 if opcode & 0x000f == 0x0000 => {
-                self.v[vx] = self.v[vy];
-                self.pc += 2;
-            },
-            0x8000 if opcode & 0x000f == 0x0001 => {
-                self.v[vx] |= self.v[vy];
-                self.pc += 2;
-            },
-            0x8000 if opcode & 0x000f == 0x0002 => {
-                self.v[vx] &= self.v[vy];
-                self.pc += 2;
-            },
-            0x8000 if opcode & 0x000f == 0x0003 => {
-                self.v[vx] ^= self.v[vy];
-                self.pc += 2;
-            },
-            0x8000 if opcode & 0x000f == 0x0004 => {
+            (0x3, _, _, _) => self.pc += if self.v[vx] == nn { 2 } else { 0 },
+            (0x4, _, _, _) => self.pc += if self.v[vx] != nn { 2 } else { 0 },
+            (0x5, _, _, _) => self.pc += if self.v[vx] == self.v[vy] { 2 } else { 0 },
+            (0x6, _, _, _) => self.v[vx] = nn,
+            (0x7, _, _, _) => self.v[vx] = self.v[vx].wrapping_add(nn),
+            (0x8, _, _, 0x0) => self.v[vx] = self.v[vy],
+            (0x8, _, _, 0x1) => self.v[vx] |= self.v[vy],
+            (0x8, _, _, 0x2) => self.v[vx] &= self.v[vy],
+            (0x8, _, _, 0x3) => self.v[vx] ^= self.v[vy],
+            (0x8, _, _, 0x4) => {
                 let (v, of) = self.v[vx].overflowing_add(self.v[vy]);
                 self.v[0xf] = if of { 1 } else { 0 };
                 self.v[vx] = v;
-                self.pc += 2;
             },
-            0x8000 if opcode & 0x000f == 0x0005 => {
+            (0x8, _, _, 0x5) => {
                 let (v, of) = self.v[vx].overflowing_sub(self.v[vy]);
                 self.v[0xf] = if of { 0 } else { 1 };
                 self.v[vx] = v;
-                self.pc += 2;
             },
-            0x8000 if opcode & 0x000f == 0x0006 => {
+            (0x8, _, _, 0x6) => {
                 self.v[0xf] = self.v[vx] & 0x01;
                 self.v[vx] >>= 1;
-                self.pc += 2;
             },
-            0x8000 if opcode & 0x000f == 0x0007 => {
+            (0x8, _, _, 0x7) => {
                 let (v, of) = self.v[vy].overflowing_sub(self.v[vx]);
                 self.v[0xf] = if of { 0 } else { 1 };
                 self.v[vx] = v;
-                self.pc += 2;
             },
-            0x8000 if opcode & 0x000f == 0x000e => {
+            (0x8, _, _, 0xe) => {
                 self.v[0xf] = self.v[vx] & 0x80;
                 self.v[vx] <<= 1;
-                self.pc += 2;
             },
-            0x9000 => {
-                self.pc += if self.v[vx] != self.v[vy] { 4 } else { 2 };
-            },
-            0xa000 => {
-                self.i = nnn;
-                self.pc += 2;
-            },
-            0xb000 => {
-                self.pc = nnn + self.v[0x0] as u16;
-            },
-            0xc000 => {
-                self.v[vx] = random::<u8>() & nn;
-                self.pc += 2;
-            },
-            0xd000 => {
+            (0x9, _, _, _) => self.pc += if self.v[vx] != self.v[vy] { 2 } else { 0 },
+            (0xa, _, _, _) => self.i = nnn,
+            (0xb, _, _, _) => self.pc = nnn + self.v[0x0] as u16,
+            (0xc, _, _, _) => self.v[vx] = random::<u8>() & nn,
+            (0xd, _, _, _) => {
                 let x = self.v[vx] as usize;
                 let y = self.v[vy] as usize;
-                let height = opcode & 0x000f;
 
                 self.v[0xf] = 0;
 
-                for line in 0..height {
+                for line in 0..(n as u16) {
                     let data = self.memory[(self.i + line) as usize];
-
+                    let y_off = ((y as u16 + line) * SCREEN_WIDTH as u16) as u16;
                     for b in 0..8 {
                         if (data & (0x80 >> b)) != 0 {
-                            let mut pixel = x as u16 + b as u16 + ((y as u16 + line) * SCREEN_WIDTH as u16) as u16;
+                            let mut pixel = x as u16 + b as u16 + y_off;
                             // Height wrapping behavior:
                             pixel %= SCREEN_SIZE as u16;
 
@@ -181,36 +141,20 @@ impl Cpu {
                 }
 
                 self.should_draw = true;
-                self.pc += 2;
             },
-            0xe000 if opcode & 0x00ff == 0x009e => {
-                self.pc += if self.key[self.v[vx] as usize] { 4 } else { 2 };
-            },
-            0xe000 if opcode & 0x00ff == 0x00a1 => {
-                self.pc += if !self.key[self.v[vx] as usize] { 4 } else { 2 };
-            },
-            0xf000 if opcode & 0x00ff == 0x0007 => {
-                self.v[vx] = self.delay_timer;
-                self.pc += 2;
-            },
-            0xf000 if opcode & 0x00ff == 0x000a => {
-                for (index, &key) in self.key.iter().enumerate() {
-                    if key {
-                        self.v[vx] = index as u8;
-                        self.pc += 2;
-                        break;
-                    }
+            (0xe, _, 0x9, 0xe) => self.pc += if self.key[self.v[vx] as usize] { 2 } else { 0 },
+            (0xe, _, 0xa, 0x1) => self.pc += if !self.key[self.v[vx] as usize] { 2 } else { 0 },
+            (0xf, _, 0x0, 0x7) => self.v[vx] = self.delay_timer,
+            (0xf, _, 0x0, 0xa) => {
+                if let Some((index, _)) = self.key.iter().enumerate().filter(|(_, &k)| k).next() {
+                    self.v[vx] = index as u8;
+                } else {
+                    self.pc -= 2;
                 }
             },
-            0xf000 if opcode & 0x00ff == 0x0015 => {
-                self.delay_timer = self.v[vx];
-                self.pc += 2;
-            },
-            0xf000 if opcode & 0x00ff == 0x0018 => {
-                self.sound_timer = self.v[vx];
-                self.pc += 2;
-            },
-            0xf000 if opcode & 0x00ff == 0x001e => {
+            (0xf, _, 0x1, 0x5) => self.delay_timer = self.v[vx],
+            (0xf, _, 0x1, 0x8) => self.sound_timer = self.v[vx],
+            (0xf, _, 0x1, 0xe) => {
                 self.sound_timer = self.v[vx];
                 self.i += self.v[vx] as u16;
                 if self.i >= MEM_SIZE as u16 {
@@ -219,31 +163,24 @@ impl Cpu {
                 } else {
                     self.v[0xf] = 0;
                 }
-                self.pc += 2;
             },
-            0xf000 if opcode & 0x00ff == 0x0029 => {
-                self.i = FONT_START as u16 + (self.v[vx] as u16 & 0xf) * 5;
-                self.pc += 2;
-            },
-            0xf000 if opcode & 0x00ff == 0x0033 => {
+            (0xf, _, 0x2, 0x9) => self.i = FONT_START as u16 + (self.v[vx] as u16 & 0xf) * 5,
+            (0xf, _, 0x3, 0x3) => {
                 self.memory[self.i as usize] = self.v[vx] / 100;
                 self.memory[self.i as usize + 1] = (self.v[vx] / 10) % 10;
                 self.memory[self.i as usize + 2] = (self.v[vx] % 100) % 10;
-                self.pc += 2;
             },
-            0xf000 if opcode & 0x00ff == 0x0055 => {
+            (0xf, _, 0x5, 0x5) => {
                 for index in 0..vx + 1 {
                     self.memory[self.i as usize + index] = self.v[index];
                 }
-                self.pc += 2;
             },
-            0xf000 if opcode & 0x00ff == 0x0065 => {
+            (0xf, _, 0x6, 0x5) => {
                 for index in 0..vx + 1 {
                     self.v[index] = self.memory[self.i as usize + index];
                 }
-                self.pc += 2;
             },
-            _ => panic!("unsupported opcode 0x{:04x}", opcode),
+            _ => panic!("unsupported opcode 0x{:04x}", op),
         }
 
         if self.delay_timer > 0 {
